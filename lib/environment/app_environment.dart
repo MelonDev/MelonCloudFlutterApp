1b562d5d51c1d7c8f5dd73c479ecc66cd674b2b2
Chaiwiwat <melondev@icloud.com>
2022-11-21 19:30:01 +0700

    อัปเดต server path

--- lib/environment/app_environment.dart
+++ lib/environment/app_environment.dart
@@ -1,5 +1,5 @@
 class AppEnvironment {
-  static String server = "meloncloud.herokuapp.com";
+  static String server = "api.melonkemo.com";
   static String token = "YFphWX-nC2NPd-hw9lEq-3ZmLRs-K1ux4r";
   static String bookApiKey = "";
 }
